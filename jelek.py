8d61eb0e8b15e8de2a289ab675c934a95564dba0
K1nimod <164170790+K1nimod@users.noreply.github.com>
2024-05-03 11:31:06 +0200

    1.

--- jelek.py
+++ jelek.py
@@ -1,1 +1,13 @@
-Jel
+class Jel:
+    def __init__(self, ora, perc, mp, x, y):
+        self.ora = ora
+        self.perc = perc
+        self.mp = mp
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return f"{self.ora} {self.perc}{self.mp}{self.x}{self.y}"
+        
+f = open("jel.txt", "rt", encoding="utf-8")
+
